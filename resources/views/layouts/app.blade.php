bb3487244481bd1780451acec12a3d402394a16f
daniel031 <danielmaldonadogutierrez031@gmail.com>
2024-05-12 14:47:10 -0400

    firstCommit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,20 @@
+@extends('adminlte::page')
+
+@section('title', 'Pagina Principal')
+
+@section('content_header')
+    <h1>Pagina Principal</h1>
+@stop
+
+@section('content')
+    <p>All for you</p>
+    <p>Bienvenido ... </p>
+@stop
+
+@section('css')
+
+@stop
+
+@section('js')
+    
+@stop
